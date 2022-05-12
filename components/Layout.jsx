29010c757e31a226cf727e5b358da13b2e38e03b
fodrix-photography <fodrixx@gmail.com>
2022-05-12 23:25:11 +0530

    favicon issue resolved

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Head from 'next/head'
 import { Header } from './'
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
+      <Head>
+        <title>Fodrix blog</title>
+        <link rel="icon" href="/fodrix-icon.png" />
+      </Head>
       {children}
     </>
   )
